b8b41f63990084a1efe29568684e7b94e6b1822f
Jason Doyle <jason.e.doyle@gmail.com>
2015-09-24 21:52:02 -0400

    Added color track code from OpenCV tutorials.

--- main.py
+++ main.py
@@ -25,17 +25,34 @@ time.sleep(0.1)
  
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+
+# insert opencv code here
+    
+    # convert from rgb to hsv
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    # show the frame
+    cv2.imshow("Frame", image)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
  
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
- 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    # if the `q` key was pressed, break from the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
