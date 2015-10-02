9ccb2584d4f49197dfb94f6bbd3adbc89e1f5cb2
Jason Doyle <jason.e.doyle@gmail.com>
2015-10-01 22:05:39 -0400

    ADD: Started new main program that works with webcamera instead of picamera.

---
+++ main_webcam.py
@@ -0,0 +1,44 @@
+# Jason Doyle
+# Phillip Benzinger
+#
+# main_webcam.py
+# main python program that uses webcamera instead of picamera.
+# 
+# Stated: Oct. 01, 2015
+# Updated:
+
+# import the necessary packages
+import time
+import cv2
+ 
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of red color in HSV
+    lower_red = np.array([110,50,50])
+    upper_red = np.array([130,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    # show the frame
+    cv2.imshow("Frame", image)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    
+ 
+    # if the `q` key was pressed, break from the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
