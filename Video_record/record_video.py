7f66cefb9fae93923ab15002c89494ce9bd8b0ea
Jason Doyle <jason.e.doyle@gmail.com>
2015-10-30 10:54:30 -0400

    MOD: Slowed video down.

--- Video_record/record_video.py
+++ Video_record/record_video.py
@@ -38,12 +38,12 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     out.write(image)
 
     # show the frame
-    cv2.imshow("Frame", image)
+    #cv2.imshow("Frame", image)
     
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
  
     # if the `q` key was pressed, break from the loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    break
 
